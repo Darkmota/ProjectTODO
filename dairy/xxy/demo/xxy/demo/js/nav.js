1974013294b5697a959e39bcec7a6afc7b0a6b66
RaconXXY <1019416676@qq.com>
2019-04-26 08:03:32 +0800

    导航栏和轮播图
    增加了导航栏和轮播图

---
+++ dairy/xxy/demo/xxy/demo/js/nav.js
@@ -0,0 +1,9 @@
+﻿window.onload = function(){
+    $(function () {
+        $('#nav_all>li').mouseenter(function () {
+            $(this).find('ul').stop(false, true).slideDown(500);
+        }).mouseleave(function () {
+            $(this).find('ul').stop(false, true).slideUp(500);
+        });
+    });
+}
