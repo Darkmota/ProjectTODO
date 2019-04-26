0ff846853fbf3be71f56b44bd27ae14202fa69b9
RaconXXY <1019416676@qq.com>
2019-04-26 22:36:56 +0800

    update
    优化了响应式布局

--- dairy/xxy/demo/js/nav.js
+++
@@ -1,9 +0,0 @@
-﻿window.onload = function(){
-    $(function () {
-        $('#nav_all>li').mouseenter(function () {
-            $(this).find('ul').stop(false, true).slideDown(500);
-        }).mouseleave(function () {
-            $(this).find('ul').stop(false, true).slideUp(500);
-        });
-    });
-}
