f14fba13a8c01081143867b3c11853275972bfd8
RaconXXY <1019416676@qq.com>
2019-05-03 04:07:41 +0800

    update

--- demo/js/handleFoldClick.js
+++ demo/js/handleFoldClick.js
@@ -1,22 +1,31 @@
-$(document).ready(function(e) {
-    $("#btn-fold").click(function(e) {
-        if($(".second-menu").css('left') === '0px'){
-            $(".second-menu").animate({left:'-80%'},600);
-            $(".main-content").animate({opacity:1},600);
-        }
-        else{
-            $(".second-menu").animate({left:'0%'},600);
-            $(".main-content").animate({opacity:0.5},600);
+$(document).ready(function (e) {
+    $("#btn-fold").click(function (e) {
+        if ($(".second-menu").css('left') === '0px') {
+            $(".second-menu").animate({
+                left: '-80%'
+            }, 600);
+            $(".main-content").animate({
+                opacity: 1
+            }, 600);
+        } else {
+            $(".second-menu").animate({
+                left: '0%'
+            }, 600);
+            $(".main-content").animate({
+                opacity: 0.5
+            }, 600);
         }
     });
 
-    $(".expand").click(function(e) {   
-        console.log($(this).attr("id"));   
-        if($(this).children("ul").css("display") === "none"){
-            $(this).children("ul").css("display","block");
+    $(".second-menu-item").click(function () {
+        if ($(this).children("ul").css("display") === "none") {
+            $(this).children("ul").slideDown();
+        } else {
+            $(this).children("ul").slideUp();
         }
-        else{
-            $(this).children("ul").css("display","none");
-        }
+    })
+
+    $(".second-nav-lan").click(function () {
+        $(".lan-select").slideToggle();
     })
 });
